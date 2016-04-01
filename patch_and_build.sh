d89d991d71280dbb8acf3aa1ffa40f3151618d18
Cat73 <1901803382@qq.com>
2016-04-01 14:08:48 +0800

    更新编译脚本的版本

--- patch_and_build.sh
+++ patch_and_build.sh
@@ -3,4 +3,4 @@ find . -name "Cat73-*.patch" | xargs rm -f
 git clone https://github.com/Cat7373/Spigot_Patchs.git ./Cat73_Spigot_Patchs/
 cp -r ./Cat73_Spigot_Patchs/Spigot/ ./
 
-java -jar BuildTools.jar --rev 1.9
+java -jar BuildTools.jar --rev 1.9.2
