4d11d7b5b0b001e868c76dce87d727dc8f538a09
Cat73 <cat7373x@gmail.com>
2018-10-06 20:40:22 +0800

    :arrow_up: 部分完成 1.13.1 升级

--- import_mc_dev.sh
+++ import_mc_dev.sh
@@ -4,7 +4,7 @@
 basedir="$(cd "$1" && pwd -P)"
 
 # nms 文件目录 # TODO 自动找目标目录，省的每个版本都要手动改
-nmsdir="$basedir/work/decompile-cf6b1333/net/minecraft/server"
+nmsdir="$basedir/work/decompile-dca1ee0d/net/minecraft/server"
 # 工作目录
 workdir="$basedir/Spigot/Spigot-Server"
 
--- patch_and_build.sh
+++ patch_and_build.sh
@@ -3,7 +3,7 @@
 rm -rf ./Cat73_Spigot_Patchs/
 find . -name "*Cat73*.patch" | xargs rm -fv
 
-java -jar BuildTools.jar --rev 1.12.2
+java -jar BuildTools.jar --rev 1.13.1
 
 git clone https://github.com/Cat7373/Spigot_Patchs.git ./Cat73_Spigot_Patchs/
 cp -rv ./Cat73_Spigot_Patchs/Spigot/ ./
@@ -12,4 +12,4 @@ cd Spigot
 bash applyPatches.sh
 cd Spigot-Server
 bash ../../apache-maven-3.5.0/bin/mvn clean package
-cp target/spigot-1.12.2-R0.1-SNAPSHOT.jar ../../spigot-cat73-1.12.2.jar
+cp target/spigot-1.13.1-R0.1-SNAPSHOT.jar ../../spigot-cat73-1.13.1.jar
