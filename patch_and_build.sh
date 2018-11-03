fc29373c94362bee9913c1cc09893d05978b7761
Cat73 <root@cat73.org>
2018-11-03 11:43:34 +0800

    :arrow_up: 升级到 1.13.2；移除会导致问题的代码

--- patch_and_build.sh
+++ patch_and_build.sh
@@ -3,13 +3,13 @@
 rm -rf ./Cat73_Spigot_Patchs/
 find . -name "*Cat73*.patch" | xargs rm -fv
 
-java -jar BuildTools.jar --rev 1.13.1
+java -jar BuildTools.jar --rev 1.13.2
 
 git clone https://github.com/Cat7373/Spigot_Patchs.git ./Cat73_Spigot_Patchs/
-cp -rv ./Cat73_Spigot_Patchs/Spigot/ ./
+cp -rv ./Cat73_Spigot_Patchs/Spigot ./
 
 cd Spigot
 bash applyPatches.sh
 cd Spigot-Server
 bash ../../apache-maven-3.5.0/bin/mvn clean package
-cp target/spigot-1.13.1-R0.1-SNAPSHOT.jar ../../spigot-cat73-1.13.1.jar
+cp target/spigot-1.13.2-R0.1-SNAPSHOT.jar ../../spigot-cat73-1.13.2.jar
